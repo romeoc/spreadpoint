cba87fb1feb4354644a4b83b84cb7a42d4748d09
Romeo Cozac <romeo_cozac@yahoo.com>
2015-10-09 00:05:07 +0300

    Adding inspectlet

--- config/autoload/plugins.global.php
+++ config/autoload/plugins.global.php
@@ -6,5 +6,11 @@ return array(
     'google-analytics' => array(
         'enabled' => false,
         'app-id' => 'UA-62958605-2',
-    )
+    ),
+    'zopim' => array(
+        'enabled' => false
+    ),
+    'inspectlet' => array(
+        'enabled' => false
+    ),
 );
