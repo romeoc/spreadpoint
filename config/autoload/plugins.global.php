5a5aa07c57fcd28dd6df6b946d5fa6b474b505d4
Romeo Cozac <romeo_cozac@yahoo.com>
2015-10-14 03:23:47 +0300

    Adding remarketing tag

--- config/autoload/plugins.global.php
+++ config/autoload/plugins.global.php
@@ -7,6 +7,10 @@ return array(
         'enabled' => false,
         'app-id' => 'UA-62958605-2',
     ),
+    'remarketing' => array(
+        'enabled' => false,
+        'conversion-id' => 948111439
+    ),
     'zopim' => array(
         'enabled' => false
     ),
